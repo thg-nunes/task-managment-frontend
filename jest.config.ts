3f742fed4775d301b4c327b329230917a520d00f
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-04-25 17:21:32 -0300

    feat: adiciona config para rodar os testes

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
+
+export default createJestConfig(config)
