dc868452dab20411bb88ea0e8013b1d0b6369373
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-07-04 14:36:18 -0300

    feat: adiciona seção de imagens ilustrativas

--- src/app/(pages)/(free_access)/login/page.tsx
+++ src/app/(pages)/(free_access)/login/page.tsx
@@ -1,3 +1,31 @@
+import type { Metadata } from 'next'
+import Image from 'next/image'
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'faça login em nossa plataforma para acessar os conteúdos privados.',
+}
+
 export default function SignIn() {
-  return <h1>Sign in</h1>
+  return (
+    <div className="grid grid-cols-2">
+      <section></section>
+      <section className="grid h-screen w-full rounded-l-full bg-violet-500 md:grid-cols-2 md:grid-rows-4">
+        <Image
+          width={232}
+          height={232}
+          src="/illustrations/people_in_pc.svg"
+          className="col-start-2 mt-3 justify-self-center"
+          alt="ilustração de um boneco em forma de pessoa, esse boneco está sentado em ma mesa"
+        />
+        <Image
+          width={464}
+          height={464}
+          src="/illustrations/completed_tasks.svg"
+          className="row-start-2 justify-self-center md:ml-40"
+          alt="ilustração de dois bonecos em forma de pessoa, elas estão segurando uma papel cada e tem outro papel grande na frente ilustrando uma lista de tarefas"
+        />
+      </section>
+    </div>
+  )
 }
