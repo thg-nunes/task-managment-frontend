400a4a5a6b349575c90acb9649430d9dfc6040cc
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-07-18 10:24:55 -0300

    chore: corrige nome da variável de api_uri

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
-  uri: `${process.env.API_URI}`,
+  uri: `${process.env.NEXT_PUBLIC_API_URI}`,
   cache: new InMemoryCache(),
 })
