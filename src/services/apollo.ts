90f19e748d31cbb1aa0f51d301df0cd4a220e58b
thiago nunes <desenvolvedor.nunes@gmail.com>
2024-07-31 14:27:11 -0300

    feat: incliui credenciais na confi do apollo-client

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -3,4 +3,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_API_URI}`,
   cache: new InMemoryCache(),
+  credentials: 'include',
 })
